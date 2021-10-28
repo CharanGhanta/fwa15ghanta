e065bb907a89b1d8d98acf12973e01920fa24524
S545515 <charanghanta.1999@gmail.com>
2021-10-27 21:12:17 -0500

    changes

---
+++ routes/computation.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams
+    if (req.method === 'GET') {
+        value = search_params.get("x")
+        if(value === null)
+        {
+            value=Math.round(Math.random()*900);
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Math.cos() applied to ' + value + ' is ' + Math.atan(value))
+        res.write('<br/>Math.asin() applied to ' + value + ' is ' + Math.exp(value))
+        res.write('<br/>Math.asinh() applied to ' + value + ' is ' + Math.expm1(value))
+        res.end()
+    }
+});
+
+module.exports = router;
